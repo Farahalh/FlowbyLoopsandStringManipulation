65826228c195591fd15ce713cc597a5e959d6435
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-15 11:10:24 +0200

    Creating the foundation of a switch statement for the menu

--- FlowbyLoopsandStringManipulation/Program.cs
+++ FlowbyLoopsandStringManipulation/Program.cs
@@ -6,11 +6,21 @@
         {
             Console.WriteLine("Welcome to the main menu!");
             Console.WriteLine("Please navigate the menu by choosing a number:");
-            Console.WriteLine("1. Enter you age: ");
+            Console.WriteLine("1. Purchase movie tickets ");
             Console.WriteLine("2. The repeat game! ");
             Console.WriteLine("3. The word game! ");
             Console.WriteLine("0. Exit");
+            string selectedAction = Console.ReadLine();
 
+            switch (selectedAction)
+            {
+                case "0":
+                    Console.WriteLine("Closing down the menu.");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input, please try again!");
+                    break;
+            }
         }
     }
 }
